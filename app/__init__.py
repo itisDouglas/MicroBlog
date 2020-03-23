569cb7ef30675e0dc4589189b0f3582bb95c4f81
Douglas Cueva <dougcueva@gmail.com>
2020-03-22 20:13:46 -0400

    began adding a model

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 #lower case config is name of the python module config.py
 #upper case Config is the class Config
 app.config.from_object(Config)
+db = SQLAlchemy(app) #represents database
+migrate = Migrate(app, db) #represents migration engine
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -2,6 +2,12 @@
 #configuration settings are defined as class variables inside the config class
 #flask uses value of the secret key as a cryptographic key useful to generate signatoures or tokens
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    #good practice to set configuration from environment variables
+    #provide fallback value when environment doens't define variable
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
