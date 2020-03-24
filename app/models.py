81b97bc0d9623f62ddcdbf8ae7956155045badfa
Douglas Cueva <dougcueva@gmail.com>
2020-03-23 20:17:06 -0400

    added additional tables

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -5,7 +6,24 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         #this tells python how to pritn objects of this class
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    """
+        REpresents blog posts written by users.
+        You'll want to work with UTC dates and times in a server app.
+        user_id is a foreign that connects with user.id
+        Backref argument defines name of filed that will be added to objects of the "many" class that points back at the "one" object.
+        the 'lazy' argument defines how db query for relationship will be issues
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post> {}'.format(self.body)
--- microblog.py
+++ microblog.py
@@ -2,5 +2,14 @@
     this script is a top-level scrip that defines the Flask Application instance.
 
     Flask application instance is called 'app' and is member of the 'app'
+
+    @app.shell_context_processor decorator registers function as a shell context function.
+    When 'flask shell' command runs it'll invoke this function.
+    It will register the items returned by it in the shell session. 
 """
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User': User, 'Post': Post}
