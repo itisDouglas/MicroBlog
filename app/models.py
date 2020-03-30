99eea701f26a3a5e87eaa1f8e80f153181d843ed
Douglas Cueva <dougcueva@gmail.com>
2020-03-30 19:16:41 -0400

    added changes to the database

--- app/models.py
+++ app/models.py
@@ -4,6 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin #flask-login provides this mixin class. works with user model
 from hashlib import md5
 
+#this is an association; only contains foreign keys of type int
+followers = db.Table('followers', 
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -28,6 +34,14 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size
         )
+
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref = db.backref('followers', lazy='dynamic'),
+        lazy = 'dynamic'
+    )
 
 class Post(db.Model):
     """
